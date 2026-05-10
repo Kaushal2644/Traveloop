c2431e43c7a967358e0e067382ee8e9892c3eeca
Kaushal Patel <kaushalpatel2506@gmail.com>
2026-05-10 09:29:02 +0530

    Setup of the project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-center mt-10" style={{color: '#2A9D8F'}}>
+        Traveloop is Working!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
